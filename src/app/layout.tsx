2c7ba927d77c29b3a72e184c0745b057043d2b83
Mel Chua <mchuam@gmail.com>
2024-12-03 11:50:20 +0700

    Updated links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,30 @@
-import type { Metadata } from "next"
-import localFont from "next/font/local"
-import "./globals.css"
-import Providers from "@/components/Providers"
-import Head from "next/head"
+import type { Metadata } from 'next'
+import Link from 'next/link'
+import localFont from 'next/font/local'
+import './globals.css'
+import Providers from '@/components/Providers'
+import Head from 'next/head'
 
 const geistSans = localFont({
-  src: "../fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: '../fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
 })
 const geistMono = localFont({
-  src: "../fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: '../fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
 })
 
 export const metadata: Metadata = {
-  title: "BadRobot AI Moderator",
-  description: "NextJS",
+  title: 'BadRobot AI Moderator',
+  description: 'NextJS',
 }
+
+const links = [
+  { url: '/', label: 'Main' },
+  { url: '/docs', label: 'API Docs' },
+]
 
 export default function RootLayout({
   children,
@@ -32,9 +38,23 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased justify-center flex p-6`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers>{children}</Providers>
+        <div className="m-4">
+          <nav>
+            <ul className="flex gap-2 justify-center">
+              {links.map((link) => {
+                return (
+                  <li>
+                    <Link href={link.url}>{link.label}</Link>
+                  </li>
+                )
+              })}
+            </ul>
+          </nav>
+
+          <Providers>{children}</Providers>
+        </div>
       </body>
     </html>
   )
